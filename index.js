ade517ddf0fe1ad285951912f052e1c8560d662c
Obleas777 <151100487+Obleas777@users.noreply.github.com>
2023-11-23 19:41:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var bandera= false;
 
 function validar() {
+    alert($('#user').val());
+
+    var user = document.getElementById("user");
+    alert("Tu usuario es:"+user.value+"Contraseña:" +password.value);
+}
+
+function validar() {
     if(bandera){
         console.log("Validacion completa");
     }
